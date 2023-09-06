645de9ada22ad1759e4b399e641f37b3a56c7895
leanciappa <ciapparelli.leandro@gmail.com>
2023-09-05 23:37:04 -0300

    V1.0

---
+++ desafio1.js
@@ -0,0 +1,39 @@
+class  ProductManager{
+    constructor(){
+        this.products = []; 
+    }
+
+    getProducts(){ 
+        console.log(products)
+        return this.products; 
+    } 
+
+    addProducts(title, description, price, thumbnail, code, stock){
+        let newId
+        if(this.products.length ==0){
+            newId=1
+        }else {
+            newId = this.products [this.products.lenght-1].id+1
+        }
+        const newProduct = {
+            id: newId, 
+            title, 
+            description, 
+            price, 
+            thumbnail, 
+            code, 
+            stock}
+        this.products.push(newProduct)
+    }
+
+    getProductById(idProducts){ 
+        const products = this.products.find(elm => elm.id === idProducts);
+        if (!products){
+            console.log("Not found");
+        } else{
+            console.log (products)
+            return products
+
+        }
+    }
+}
